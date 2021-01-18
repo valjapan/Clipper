58883af97b2f3a3cca43a5e72ff2b935141f71d2
valjapan <yukinabe3561@icloud.com>
2021-01-18 12:24:42 +0900

    add fragment transaction

--- app/src/main/java/com/valjapan/clipper/MainActivity.kt
+++ app/src/main/java/com/valjapan/clipper/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.valjapan.clipper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.fragment_container, HistoryFragment()).commit()
+        }
     }
 }
